9ad7099d08fbcfa5327d9b87e116129a5fd84e96
Lucas Couto <lucas3couto@gmail.com>
2020-06-03 19:29:31 -0300

    repage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { Router } from 'react-router-dom'
-import Routes from './routes'
-import history from './services/history'
-import GlobalStyles from './styles/global'
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from "react-redux";
-import { store, persistor } from "./store";
-import { ToastContainer } from "react-toastify";
 
-const App = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
+import './config/ReactotronConfig';
+
+import { ToastContainer } from 'react-toastify';
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyles from './styles/global';
+
+import { store, persistor } from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <ThemeProvider theme={theme}>
-      <Router history={history}>
-          <Routes />
-          <GlobalStyles />
-          <ToastContainer autoClose={3000} />
-      </Router>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyles />
+                <ToastContainer autoClose={3000} />
+            </Router>
         </ThemeProvider>
-    </PersistGate>
-  </Provider>
-)
+      </PersistGate>
+    </Provider>
+  );
+}
 
-export default App
+export default App;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,49 +1,47 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
-  *{
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
+
+  * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
-    font-family: 'Nunito', sans-serif;
   }
+
   *:focus {
     outline: 0;
   }
-  html, body, #root{
+
+  html, body, #root {
     height: 100%;
   }
-  body{
+
+  body {
     -webkit-font-smoothing: antialiased;
   }
-  a{
-    text-decoration:none;
-    color: inherit;
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
   }
-  ul{
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
     list-style: none;
   }
-  button{
+
+  button {
     cursor: pointer;
   }
 
-  h1, h2, h3 {
-    color: ${props => props.theme.palette.black};
-  } 
-
-  svg{
-    color: ${props => props.theme.palette.purple.main}
-  }
-
-  p{
-    color: ${props => props.theme.palette.grey.main.light};
-  }
-
-  .rdw-editor-wrapper {
+.rdw-editor-wrapper {
     border: 1px solid ${props => props.theme.palette.background} !important;
     border-radius: 5px;
     padding: 3px;
