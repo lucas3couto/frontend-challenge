d625872f8b9abe0133d107c82cd216a1d5ffee17
Lucas Couto <lucas3couto@gmail.com>
2020-06-06 10:30:09 -0300

    cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:3333",
+    headers: {
+        'Access-Control-Allow-Credentials':true,
+    }
 });
 
 export default api
